4240c4ee4927e7657cae66427e8420b7c8e7a8cc
johnsachs1 <97467168+johnsachs1@users.noreply.github.com>
2022-03-31 14:44:44 -0400

    Add files via upload

---
+++ Blog Project/TechTalentBlog/src/main/java/com/techtalentsouth/TechTalentBlog/BlogPost/BlogPostController.java
@@ -0,0 +1,37 @@
+package com.techtalentsouth.TechTalentBlog.BlogPost;
+
+import com.techtalentsouth.TechTalentBlog.BlogPost.BlogPost;
+import com.techtalentsouth.TechTalentBlog.BlogPost.BlogPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BlogPostController {
+
+    @Autowired
+    private BlogPostRepository blogPostRepository;
+    private static List <BlogPost> posts = new ArrayList <>();
+
+    @GetMapping(value="/")
+    public String index(BlogPost blogPost, Model model) {
+        model.addAttribute("posts", posts);
+        return "blogpost/index";
+    }
+
+    private BlogPost blogPost;
+
+    @PostMapping(value = "/")
+    public String addNewBlogPost(BlogPost blogPost, Model model) {
+        blogPostRepository.save(blogPost);
+        posts.add(blogPost);
+        model.addAttribute("title", blogPost.getTitle());
+        model.addAttribute("author", blogPost.getAuthor());
+        model.addAttribute("blogEntry", blogPost.getBlogEntry());
+        return "blogpost/result";
+    }
+
+}
+
